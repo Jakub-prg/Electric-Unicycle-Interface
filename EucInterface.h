8753dc0b19a73313451b8f90441c23a6c436f959
Jakub-prg <71125009+Jakub-prg@users.noreply.github.com>
2021-04-17 00:24:27 +0200

    Update EucInterface.h

--- EucInterface.h
+++ EucInterface.h
@@ -17,13 +17,10 @@ public:
     unsigned char tempMileage[4] = {0x00, 0x00, 0x00, 0x00};
     unsigned char current[2] = {0x00, 0x00};
     unsigned char temperature[2] = {0x00, 0x00};
-    //unsigned char unknownData[4]; // TODO: figure out what that is
-    //unsigned char headerSecondaryPacket[8] = {0x00, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
     unsigned char unknownData[4] = {0x00, 0x00, 0x00, 0x00}; // TODO: figure out what that is //NOTE: only first byte seems to change
     unsigned char headerSecondaryPacket[8] = {0x00, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
     unsigned char mileage[4] = {0x00, 0x00, 0x00, 0x00};
-    unsigned char end[12] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    
+    //unsigned char end[12] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     bool dataIsNew = false;
   };
   
@@ -33,14 +30,14 @@ public:
     float tempMileage;
     float current;
     float temperature;
-    //unsigned char unknownData[4];
+    float battery;
     float mileage;
     bool dataIsNew = false;
   };
   
   Stream &ReceiverSerial;
   Stream &TransmitterSerial;
-  void (*eucLoop)(float,float,float,float,float,float,bool);
+  void (*eucLoop)(float,float,float,float,float,float,float,bool);
   
   Euc::RawData receiveRawData();
   Euc::UsableData makeRawDataUsable(Euc::RawData eucRawData);
@@ -48,7 +45,7 @@ public:
   Euc(Stream &ReceiverSerial, Stream &TransmitterSerial);
   
   void tick();
-  void setCallback(void (*eucLoopCallback)(float,float,float,float,float,float,bool));
+  void setCallback(void (*eucLoopCallback)(float,float,float,float,float,float,float,bool));
   
   void beep();
   void maddenMode();
